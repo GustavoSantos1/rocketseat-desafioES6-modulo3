878cab1a6628f1edced8f9c45236bf98069236a7
Gustavo Santos <31597729+GustavoSantos1@users.noreply.github.com>
2020-03-09 16:49:53 -0300

    Add files via upload

---
+++ ex3.js
@@ -0,0 +1,13 @@
+class Github {
+    static async getRepositories(repo) {
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repo}`)
+            console.log(response.data);
+        }
+        catch (err) {
+            console.log('Repositório não existe');
+        }
+    }
+}
+Github.getRepositories('rocketseat/rocketseat.com.br');
+Github.getRepositories('rocketseat/dslkvmskv');
